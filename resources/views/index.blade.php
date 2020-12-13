b7f68332fda08d5c1df40b5d7d3ac7341e0c384f
Pablo Diaz <pabloandi@gmail.com>
2020-12-13 17:17:50 -0500

    Lesson complete

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="/css/app.css">
+
+    </head>
+    <body>
+        <div class="card parent">
+            <div class="child">
+                <h1>Some Heading</h1>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+                <p>Lorem ipsum dolor sit amet consectetur.</p>
+
+                <button>click me</button>
+            </div>
+
+        </div>
+    </body>
+</html>
