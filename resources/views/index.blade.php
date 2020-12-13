aaa57a87b7503f1f756e541bf8483314023da07e
Pablo Diaz <pabloandi@gmail.com>
2020-12-13 18:00:10 -0500

    Lesson complete

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,19 +6,27 @@
 
         <title>Laravel</title>
 
+        <link
+            href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
+            rel="stylesheet"
+        />
+
         <link rel="stylesheet" href="/css/app.css">
 
     </head>
-    <body>
-        <div class="card parent">
-            <div class="child">
-                <h1>Some Heading</h1>
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
-                <p>Lorem ipsum dolor sit amet consectetur.</p>
+   <body class="py-4">
+       <div class="max-w-lg mx-auto bg-gray-300 p-4">
+           @foreach (range(1,10) as $i)
+               <div class="bg-white p-4 rounded mb-4">
+                   <h1 class="font-bold mb-2 text-lg">A new post</h1>
+                   <p class="text-sm clamp two-lines lg:three-lines">
+                       @php
+                           echo str_repeat('Lorem ipsum dolor, sit amet consectetur adipisicing elit. ', $i)
+                       @endphp
+                   </p>
 
-                <button>click me</button>
-            </div>
-
-        </div>
-    </body>
+               </div>
+           @endforeach
+       </div>
+   </body>
 </html>
